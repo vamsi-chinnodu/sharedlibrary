549074d2763e89e660dd04e881c5c52352ef2582
vamsi-chinnodu <vamsimacharla0150@gmail.com>
2024-11-21 15:50:31 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def buildArtifact()
 {
  sh "mvn package"
 }
+def deployTomcat(jobname,ipaddress,context)
+{
+ sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${context}.war"
+}
